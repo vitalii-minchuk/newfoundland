ec22b7dede4e697a7f7438376a4fd3e1a5c61dc4
vitalii-minchuk <mcmin4doutsch@gmail.com>
2024-03-11 08:13:51 +0200

    add auth with github and google

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,4 +1,6 @@
 import Credentials from "next-auth/providers/credentials"
+import GitHub from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
 import bcrypt from "bcryptjs"
 
 import type { NextAuthConfig } from "next-auth"
@@ -7,6 +9,14 @@ import { getUserByEmail } from "@/helpers/user"
 
 export default {
   providers: [
+    GitHub({
+        clientId: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    Google({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     Credentials({
         async authorize(credentials) {
             const validatedData = LoginSchema.safeParse(credentials)
--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,10 @@
-import NextAuth from "next-auth"
+import NextAuth, { DefaultSession } from "next-auth"
 import {PrismaAdapter} from '@auth/prisma-adapter'
+
+import { UserRole } from "@prisma/client"
 import { prismaDB } from "@/lib/db"
 import authConfig from '@/auth.config'
+import { getUserById } from "@/helpers/user"
 
 export const {
   handlers: { GET, POST },
@@ -10,14 +13,35 @@ export const {
   signOut,
 } = NextAuth({
   callbacks: {
+    // async signIn({user}) {
+    //   if (!user?.id) return false
+
+    //   const existingUser = await getUserById(user.id)
+
+    //   if (!existingUser || !existingUser.emailVerified) return false
+
+    //   return true
+    // },
     async session({session, token}) {
-      if (token?.sub && session?.user?.id) {
+      if (token?.sub && session?.user) {
         session.user.id = token.sub
       }
-      
+
+      if (token?.role && session?.user) {
+        session.user.role = token.role as UserRole
+      }
+
       return session
     },
     async jwt({token}) {
+      if (!token.sub) return token
+
+      const user = await getUserById(token.sub)
+
+      if (!user) return token
+
+      token.role = user.role
+
       return token
     },
   },
